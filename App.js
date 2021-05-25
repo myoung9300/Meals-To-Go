d9363f5d34e90dd7ba6fad652b23032a20c8e2f6
Matt Young <myoung9300@yahoo.com>
2021-05-25 14:02:10 -0600

    error fixing

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import React, {Component} from 'react';
-import { StatusBar, StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{flex: 1, backgroundColor: "purple", marginTop: StatusBar.currentHeight}}>
-        <View style={{ padding: 16, backgroundColor: "yellow"}}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
           <Text>Search</Text>
         </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "pink"}}>
+        <View style={styles.list}>
           <Text>List</Text>
         </View>
       </SafeAreaView>
@@ -19,4 +19,19 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor: 'purple', 
+    marginTop: StatusBar.currentHeight
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "yellow",
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "pink",
+  }
+});
