fafd3c78ad7264a0d1ffe454d2c158821e6fcc6f
Matt Young <myoung9300@yahoo.com>
2021-05-25 12:24:19 -0600

    Set up for ios and android

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import React, {Component} from 'react';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: 'center'}}>
-      <Text>Lets get STARTED!</Text>
-    </View>
+    <>
+      <SafeAreaView style={{flex: 1, backgroundColor: "purple", marginTop: StatusBar.currentHeight}}>
+        <View style={{ padding: 16, backgroundColor: "yellow"}}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "pink"}}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
